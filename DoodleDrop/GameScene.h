ecff2a9d670830abfd939d2364f0e61fd1ca26df
happyjiahan <happyjiahan@gmail.com>
2013-03-10 14:12:52 +0800

    add 2 files
    a

---
+++ DoodleDrop/GameScene.h
@@ -0,0 +1,16 @@
+//
+//  GameScene.h
+//  DoodleDrop
+//
+//  Created by lijiahan on 13-3-10.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameScene : GameScene {
+    
+}
+
+@end
---
+++ DoodleDrop/GameScene.m
@@ -0,0 +1,14 @@
+//
+//  GameScene.m
+//  DoodleDrop
+//
+//  Created by lijiahan on 13-3-10.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameScene.h"
+
+
+@implementation GameScene
+
+@end
